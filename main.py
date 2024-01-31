9abb0c611074e917d98b96be8733a95d5deeb991
Vadym Malitskyi <vadym.malitsky357@gmail.com>
2024-01-31 19:27:34 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import math_operations as mo
+import text_operations as to
+import numpy
+
+
+print(mo.addition(5, 10))
+print(mo.subtraction(10, 5))
+print(mo.multiplication(2, 3))
+print(mo.calculate_log(2, 10))
+print(to.count_words("This is a sentence with 7 words."))
+print(numpy.mean([1, 2, 3, 4, 5]))
---
+++ math_operations/__init__.py
@@ -0,0 +1,3 @@
+from .advanced_ops import multiplication
+from .math_operations import addition, subtraction
+from .log_operations import calculate_log
